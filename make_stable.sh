OP-TEE/manifest

commit f7824ba5119e9e36f8335983fbbd04a347ba601d

make_stable: use OP-TEE tag for linaro-swg linux repository
Change script to use OP-TEE release tag for linaro-swg github Linux
kernel repository when applicable to make sure stable release are
referenced with a immutable reference of the kernel source tree.

This change assumes OP-TEE release tag is also created for that
repository.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Acked-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>

make_stable.sh
@@ -53,5 +53,6 @@ do
 		sed "s/\(linaro-swg\/optee_examples.git\"\)/\1 revision=\"refs\/tags\/${VERSION}\" clone-depth=\"1\"/" |
 		sed "s/\(linaro-swg\/optee_benchmark.git\)revision.*/\1\/>/" | # Removes old revision
 		sed "s/\(linaro-swg\/optee_benchmark.git\"\)/\1 revision=\"refs\/tags\/${VERSION}\" clone-depth=\"1\"/" |
+		sed "s/\(linaro-swg\/linux.git\" *\)revision=\"optee\".*/\1revision=\"refs\/tags\/optee-${VERSION}\" clone-depth=\"1\"/" |
 		tee ${FILE} 2>&1 > /dev/null
 done

commit aaeae8ff30804e07d7b8e53d0f36c91b9c2b3201

make_stable.sh: add missing closing tag
When make_stable.sh replaces the linux project entry with a new one
pointing to a release tag, it lacks an XML closing tag '/>' which
causes an error with 'repo sync':

 $ repo sync optee*
 fatal: error parsing manifest /home/jerome/work/optee_repo_qemu/.repo/manifest.xml: not well-formed (invalid token): line 19, column 8

Add the missing bit.

Fixes: f7824ba5119e ("make_stable: use OP-TEE tag for linaro-swg linux repository")
Signed-off-by: Jerome Forissier <jerome@forissier.org>
Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
Change-Id: I4a1c13c7a9aa6041c39d1832f5cb3e8fffa6ab8e

make_stable.sh
@@ -53,6 +53,6 @@ do
 		sed "s/\(linaro-swg\/optee_examples.git\"\)/\1 revision=\"refs\/tags\/${VERSION}\" clone-depth=\"1\"/" |
 		sed "s/\(linaro-swg\/optee_benchmark.git\)revision.*/\1\/>/" | # Removes old revision
 		sed "s/\(linaro-swg\/optee_benchmark.git\"\)/\1 revision=\"refs\/tags\/${VERSION}\" clone-depth=\"1\"/" |
-		sed "s/\(linaro-swg\/linux.git\" *\)revision=\"optee\".*/\1revision=\"refs\/tags\/optee-${VERSION}\" clone-depth=\"1\"/" |
+		sed "s/\(linaro-swg\/linux.git\" *\)revision=\"optee\".*/\1revision=\"refs\/tags\/optee-${VERSION}\" clone-depth=\"1\" \/>/" |
 		tee ${FILE} 2>&1 > /dev/null
 done